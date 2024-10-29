8fe49a442214f433f73bf434913186e44e6f3ca6
GreatInterface <C2931597944@outlook.com>
2024-10-29 17:06:13 +0800

    新功能 : AsyncAction_ExperienceReady

---
+++ Source/Abyss/Private/GameModes/Experience/AsyncAction_ExperienceReady.cpp
@@ -0,0 +1,81 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameModes/Experience/AsyncAction_ExperienceReady.h"
+
+#include "GameModes/Experience/AbyssExperienceManagerComponent.h"
+
+
+UAsyncAction_ExperienceReady* UAsyncAction_ExperienceReady::WaitForExperienceReady(UObject* WorldContextObject)
+{
+	UAsyncAction_ExperienceReady* Action = nullptr;
+
+	if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+	{
+		Action = NewObject<UAsyncAction_ExperienceReady>();
+		Action->WorldPtr = World;
+		Action->RegisterWithGameInstance(World);
+	}
+
+	return Action;
+}
+
+void UAsyncAction_ExperienceReady::Activate()
+{
+	if (UWorld* World = WorldPtr.Get())
+	{
+		if (AGameStateBase* GS = World->GetGameState())
+		{
+			Step2_ListenToExperienceLoading(GS);
+		}
+		else
+		{
+			World->GameStateSetEvent.AddUObject(this, &ThisClass::Step1_HandleGameStateSet);
+		}
+	}
+	else
+	{
+		SetReadyToDestroy();
+	}
+}
+
+void UAsyncAction_ExperienceReady::Step1_HandleGameStateSet(AGameStateBase* GameState)
+{
+	if (UWorld* World = WorldPtr.Get())
+	{
+		World->GameStateSetEvent.RemoveAll(this);
+	}
+
+	Step2_ListenToExperienceLoading(GameState);
+}
+
+void UAsyncAction_ExperienceReady::Step2_ListenToExperienceLoading(AGameStateBase* GameState)
+{
+	check(GameState);
+	UAbyssExperienceManagerComponent* ExperienceComp = GameState->FindComponentByClass<UAbyssExperienceManagerComponent>();
+	check(ExperienceComp);
+
+	if (ExperienceComp->IsExperienceLoaded())
+	{
+		UWorld* World = WorldPtr.Get();
+		check(World);
+
+		World->GetTimerManager().SetTimerForNextTick(FTimerDelegate::CreateUObject(this, &ThisClass::Step4_BroadcastReady));
+	}
+	else
+	{
+		ExperienceComp->CallOrRegister_OnExperienceLoaded(FOnAbyssExperienceLoaded::FDelegate::CreateUObject(this, &ThisClass::Step3_HandleExperienceLoaded));
+	}
+}
+
+void UAsyncAction_ExperienceReady::Step3_HandleExperienceLoaded(const UAbyssExperienceDefinition* CurrentExperience)
+{
+	Step4_BroadcastReady();
+}
+
+void UAsyncAction_ExperienceReady::Step4_BroadcastReady()
+{
+	OnReady.Broadcast();
+	
+	SetReadyToDestroy();
+}
---
+++ Source/Abyss/Public/GameModes/Experience/AsyncAction_ExperienceReady.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "AsyncAction_ExperienceReady.generated.h"
+
+
+class UAbyssExperienceDefinition;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FExperienceReadyAsyncDelegate);
+
+UCLASS()
+class UAsyncAction_ExperienceReady : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, meta=(WorldContext="WorldContextObject", BlueprintInternalUseOnly=true))
+	static UAsyncAction_ExperienceReady* WaitForExperienceReady(UObject* WorldContextObject);
+
+	virtual void Activate() override;
+public:
+
+	UPROPERTY(BlueprintAssignable)
+	FExperienceReadyAsyncDelegate OnReady;
+	
+private:
+
+	void Step1_HandleGameStateSet(AGameStateBase* GameState);
+	void Step2_ListenToExperienceLoading(AGameStateBase* GameState);
+	void Step3_HandleExperienceLoaded(const UAbyssExperienceDefinition* CurrentExperience);
+	void Step4_BroadcastReady();
+	
+	TWeakObjectPtr<UWorld> WorldPtr;
+};
