d898dfdcbfe749e2b67be0362c63789be14d8754
GreatInterface <C2931597944@outlook.com>
2024-11-02 15:52:11 +0800

    IndicatorLayer

---
+++ Source/Abyss/Private/UI/IndicatorSystem/IndicatorLayer.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/IndicatorSystem/IndicatorLayer.h"
+
+#include "UI/IndicatorSystem/Slate/SActorCanvas.h"
+
+UIndicatorLayer::UIndicatorLayer(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	bIsVariable = true;
+	SetVisibility(ESlateVisibility::HitTestInvisible);
+}
+
+void UIndicatorLayer::ReleaseSlateResources(bool bReleaseChildren)
+{
+	Super::ReleaseSlateResources(bReleaseChildren);
+
+	MyActorCanvas.Reset();
+}
+
+TSharedRef<SWidget> UIndicatorLayer::RebuildWidget()
+{
+	if (!IsDesignTime())
+	{
+		ULocalPlayer* LP = GetOwningLocalPlayer();
+		if (ensureMsgf(LP, TEXT("Attempting to rebuild a UActorCanvas without a valid LocalPlayer!")))
+		{
+			MyActorCanvas = SNew(SActorCanvas, FLocalPlayerContext(LP), &ArrowBrush);
+			return MyActorCanvas.ToSharedRef();
+		}
+	}
+
+	return SNew(SBox);
+}
---
+++ Source/Abyss/Public/UI/IndicatorSystem/IndicatorLayer.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Widget.h"
+#include "IndicatorLayer.generated.h"
+
+class SActorCanvas;
+/**
+ * 
+ */
+UCLASS()
+class ABYSS_API UIndicatorLayer : public UWidget
+{
+	GENERATED_BODY()
+
+public:
+	UIndicatorLayer(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Appearance)
+	FSlateBrush ArrowBrush;
+
+protected:
+
+	virtual void ReleaseSlateResources(bool bReleaseChildren) override;
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+	
+protected:
+	
+	TSharedPtr<SActorCanvas> MyActorCanvas;
+};
